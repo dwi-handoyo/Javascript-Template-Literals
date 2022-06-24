c92c72d93c326efe146757e49e971af6641ab038
Dwi Handoyo <107865228+dwi-handoyo@users.noreply.github.com>
2022-06-25 06:46:07 +0700

    Create multi-line-strings.js

---
+++ multi-line-strings.js
@@ -0,0 +1,14 @@
+// without back tick, escape line (\n) is required:
+
+console.log('string 1\n string 2'); 
+// result:
+// "string 1
+//  string 2"
+
+// with back tick no need escape line (\n) needed, just type the strings into different lines:
+
+console.log(`string 1
+ string 2`); 
+// result:
+// "string 1
+//  string 2"
